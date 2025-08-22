61f4247be0d5e8afd1f0e6091dfd4904578141d6
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2025-08-22 12:23:42 -0600

    I am a SUPER MORON!

--- Spring Data/Tarea 1/tarea1/src/main/java/org/smartphones/tarea1/exception/SmartPhoneException.java
+++
@@ -1,7 +0,0 @@
-package org.smartphones.tarea1.exception;
-
-public class SmartPhoneException extends RuntimeException {
-    public SmartPhoneException(String message) {
-        super(message);
-    }
-}
