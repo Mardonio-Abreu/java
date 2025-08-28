596d476128196f1eef1e75b7adfe345262569fbb
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2025-08-27 19:28:47 -0600

    Repository with JpaRepository, derived and native queries (all tests passed)
    Signed-off-by: Mardonio-Abreu <mardonio.abreu@protonmail.com>
    
    # Conflicts:
    #	Spring Data/Tarea 1/tarea1/src/test/java/org/smartphones/tarea1/Tarea1ApplicationTests.java

--- Spring Data/Tareas/Tarea 2/tarea2/src/main/java/org/smartphones/tarea2/repository/SmartphoneRepository.java
+++ Spring Data/Tareas/Tarea 2/tarea2/src/main/java/org/smartphones/tarea2/repository/SmartphoneRepository.java
@@ -1,10 +1,8 @@
 package org.smartphones.tarea1.repository;
 
 import org.smartphones.tarea1.model.SmartPhone;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
